e6c9dd4da4221edf6417510feb9838cffa5abd2c
Estefania <estefi.95.c@gmail.com>
2019-01-28 19:02:20 +0100

    Aplicació de Jugadors feta, hi han les classes Jugador, Skin i SkinComprada

--- speedQuizProject/jugadors/models.py
+++ speedQuizProject/jugadors/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Jugador(models.Model):
+    nomJugador = models.CharField(max_length=50, primary_key=True)
+    numFormatges = models.IntegerField()
+    numFormatgets = models.IntegerField()
+    email = models.CharField(200)
+    password = models.CharField(50)
+    def __str__(self):
+        return self.nomJugador
+
+class Skin(models.Model):
+    nomSkin = models.CharField(max_length=50)
+    preu = models.IntegerField()
+    def __str__(self):
+        return self.nomSkin
+
+class SkinComprada(models.Model):
+    nomJugador = models.ForeignKey(Jugador, primary_key=True)
+    nomSkin = models.ForeignKey(Skin, primary_key=True)
+    posada = models.BooleanField()
