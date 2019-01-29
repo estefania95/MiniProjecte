e3a8177cd14247a050e8fb93b7730c26fa398307
EnyaMol <onaienya@gmail.com>
2019-01-29 18:31:20 +0100

    Urls i views de la Benvinguda funcionals

--- speedQuizProject/jugadors/views.py
+++ speedQuizProject/jugadors/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def benvinguda(request):
+    return render(request, 'benvinguda/index.html', {})
--- speedQuizProject/speedQuizProject/urls.py
+++ speedQuizProject/speedQuizProject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('benvinguda/index.html'),
+    path('', include('jugadors.urls')),
 ]
