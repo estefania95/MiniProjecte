d8fe2775c03f268131867157bb4598f45917fa39
EnyaMol <onaienya@gmail.com>
2019-01-29 19:28:38 +0100

    Urls i views funcionals

--- speedQuizProject/jugadors/urls.py
+++ speedQuizProject/jugadors/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.benvinguda, name='benvinguda'),
+    path('botiga/', views.botiga, name='botiga'),
+    path('home/', views.home, name='home'),
+    path('iniciSessio/', views.iniciSessio, name='iniciSessio'),
+    path('joc/', views.joc, name='joc'),
+    path('puntuacio/', views.puntuacio, name='puntuacio'),
+    path('registre/', views.registre, name='registre'),
 ]
--- speedQuizProject/jugadors/views.py
+++ speedQuizProject/jugadors/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def benvinguda(request):
     return render(request, 'benvinguda/index.html', {})
+
+def botiga(request):
+    return render(request, 'botiga/botiga.html', {})
+
+def home(request):
+    return render(request, 'home/home.html', {})
+
+def iniciSessio(request):
+    return render(request, 'iniciarSessio/iniciarSessio.html', {})
+
+def joc(request):
+    return render(request, 'joc/unJugador.html', {})
+
+def puntuacio(request):
+    return render(request, 'puntuacio/puntuacio.html', {})
+
+def registre(request):
+    return render(request, 'registre/registrarse.html', {})
